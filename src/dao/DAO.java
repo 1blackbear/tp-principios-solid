cc72c8d71ea4a14e173a83a72e345b2602a9e710
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2021-12-07 15:18:35 -0300

    DAO

---
+++ src/dao/DAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+	
+	public void add(T p);
+	public T get(T p);
+	public void update(T p);
+	public void delete(T p);
+	public List<T> getAll();
+
+}
